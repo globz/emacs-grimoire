1f4368799d2441133fa6d4265c1466a99dfeaeba
globz <globz@kittybomber.com>
2023-10-08 16:17:53 -0400

    testing workflow...

--- reagents/make-emacs/make_emacs.sh
+++ reagents/make-emacs/make_emacs.sh
@@ -18,8 +18,9 @@ make_emacs()
   echo "The following options are available, please make a choice ::"
   echo -e "\e[0;35m1 - Download Emacs source\e[m"
   echo -e "\e[0;35m2 - Build Emacs from source\e[m"
-  echo -e "\e[0;35m3 - Uninstall Emacs from source\e[m"
-  echo -e "\e[0;35m4 - List available Emacs builds\e[m"
+  echo -e "\e[0;35m3 - Install Emacs from source\e[m"
+  echo -e "\e[0;35m4 - Uninstall Emacs from source\e[m"
+  echo -e "\e[0;35m5 - List available Emacs builds\e[m"
 
   if [[ ! -z "$predefined_selection" ]]
   then
@@ -41,11 +42,35 @@ make_emacs()
   
   if [ "$ACTION" == 2 ]
   then
-    spellpouch -p "dialog_prompt" -e "You are about to build & install Emacs from source, press any key to continue or C-c to abort..."
-    spellpouch -p "dialog_prompt" -e "Please make sure to uninstall your current Emacs version before proceeding, press any key to continue or C-c to abort..."
+      spellpouch -p "dialog_prompt" -e "You are about to build Emacs from source, press any key to continue or C-c to abort..."
+      spellpouch -p "dialog_prompt" -e "!WARNING! You are building the version ${version} defined in this script, press any key to continue or C-c to abort..."
+
+      echo -e "\033[31m Installing build dependencies...\e[m"
+      # sudo apt build-dep -y emacs
+      # sudo apt install libgccjit0 libgccjit-11-dev libjansson4 libjansson-dev \
+      #      gnutls-bin libtree-sitter-dev gcc-11 imagemagick libmagick++-dev \
+      #      libwebp-dev webp libxft-dev libxft2
+
+      echo -e "\033[31m Configuring Emacs build...\e[m"
+      # cd "${emacs_src_dir}/emacs-${version}/"
+      # export CC=/usr/bin/gcc-11 && export CXX=/usr/bin/gcc-11
+      # ./autogen.sh
+      # ./configure --with-native-compilation=aot --with-json --with-tree-sitter
+      # make -j$(nproc)
+
+      echo -e "\033[31m Build complete, you may test this build within this directory ${emacs_src_dir}/emacs-${version}/ with ./src/emacs -Q\e[m"  
   fi
 
   if [ "$ACTION" == 3 ]
+  then
+      spellpouch -p "dialog_prompt" -e "You are about to install Emacs from source, press any key to continue or C-c to abort..."
+      spellpouch -p "dialog_prompt" -e "Please make sure to uninstall your current Emacs version before proceeding, press any key to continue or C-c to abort..."
+      spellpouch -p "dialog_prompt" -e "!WARNING! You are installing the version ${version} defined in this script, press any key to continue or C-c to abort..."
+
+      cd "${emacs_src_dir}/emacs-${version}/" && sudo make install
+  fi
+
+  if [ "$ACTION" == 4 ]
   then
       spellpouch -p "dialog_prompt" -e "You are about to uninstall Emacs from source, press any key to continue or C-c to abort..."
       spellpouch -p "dialog_prompt" -e "Please select a version from the list below..., press any key to continue or C-c to abort..."
@@ -59,14 +84,15 @@ make_emacs()
       cd -
       spellpouch -p "dialog_prompt" -e "Backup completed, do you wish to proceed and uninstall ${UNINSTALL_THIS_VERSION}?, press any key to continue or C-c to abort..."
 
-      #cd "${emacs_src_dir}/${UNINSTALL_THIS_VERSION}" && make uninstall
+      cd "${emacs_src_dir}/${UNINSTALL_THIS_VERSION}" && make uninstall
   fi
 
-  if [ "$ACTION" == 4 ]
+  if [ "$ACTION" == 5 ]
   then
       cd "${emacs_src_dir}" && ls -d emacs-*
-      echo -e "\033[31m You may build & install Emacs with witchesbrew mix make-emacs -e LINUX 2.\e[m"
-      echo -e "\033[31m You may uninstall Emacs with witchesbrew mix make-emacs -e LINUX 3.\e[m"
+      echo -e "\033[31m You may build Emacs from source with witchesbrew mix make-emacs -e LINUX 2.\e[m"
+      echo -e "\033[31m You may install Emacs from source with witchesbrew mix make-emacs -e LINUX 3.\e[m"
+      echo -e "\033[31m You may uninstall Emacs with witchesbrew mix make-emacs -e LINUX 4.\e[m"
   fi
 
 }
