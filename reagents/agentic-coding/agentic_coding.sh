6379a627a46a0d6da9efeb152a5e9d041427b78c
globz <globz@kittybomber.com>
2026-03-02 17:07:11 -0500

    Updated nono to version 0.8.0 and added nono profile for opencode

--- reagents/agentic-coding/agentic_coding.sh
+++ reagents/agentic-coding/agentic_coding.sh
@@ -2,7 +2,7 @@
 
 # This is my current agentic coding stack
 # opencode - https://github.com/anomalyco/opencode
-# non for kernel-level sandboxing - https://github.com/always-further/nono
+# nono for kernel-level sandboxing - https://github.com/always-further/nono
 
 # The following scripts tools are an alternative to nono
 # landrun for kernel-level sandboxing - https://github.com/Zouuup/landrun
@@ -11,30 +11,33 @@
 # Requirements
 # Linux kernel 5.13 or later with Landlock enabled
 # Linux kernel 6.7 or later for network restrictions (TCP bind/connect)
-# Go 1.18 or later (for building from source)
+# Go 1.18 or later (for building landrun from source)
 
 agentic_coding()
 {
 
     local wd=$(pwd)
     local emacs_deps_dir="${wd}/emacs-deps"
-    local agentic_coding_path="${wd}/reagents/agentic-coding"
+    local this_root_path="${wd}/reagents/agentic-coding"
+    local opencode_config="${this_root_path}/config/opencode/opencode.json"
+    local nono_opencode_config="${this_root_path}/config/nono/opencode.json"
+    local nono_profiles_dest="${HOME}/.config/nono/profiles/"
     local scoder_path="${wd}/reagents/agentic-coding/scoder"
     local repo_landrun="https://github.com/zouuup/landrun.git"
-    local LANDLOCK_ABI_CHECK_DEST="${HOME}/.local/bin/"
-    local NONO_VERSION="v0.7.0"
-    local NONO_DOWNLOAD_URL="https://github.com/always-further/nono/releases/download/${NONO_VERSION}/nono-${NONO_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-    local NONO_BINARY="nono-${NONO_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+    local landlock_abi_check_dest="${HOME}/.local/bin/"
+    local nono_version="v0.8.0"
+    local nono_download_url="https://github.com/always-further/nono/releases/download/${nono_version}/nono-${nono_version}-x86_64-unknown-linux-gnu.tar.gz"
+    local nono_binary="nono-${nono_version}-x86_64-unknown-linux-gnu.tar.gz"
     
     # Sourcing spellpouch from working directory (user grimoire)
     source "${wd}/spells/spellpouch.sh"
 
     echo "The following options are available, please make a choice ::"
     echo -e "\e[0;35m1 - Install opencode (harness)\e[m"
-    echo -e "\e[0;35m2 - Install nono (kernel-level sandboxing)\e[m"
-    echo -e "\e[0;35m3 - Install landrun (kernel-level sandboxing)\e[m"
-    echo -e "\e[0;35m4 - Install/Update scoder (opencode sandboxing env)\e[m"
-    echo -e "\e[0;35m5 - Install/Update opencode.json config\e[m"
+    echo -e "\e[0;35m2 - Install nono (kernel-level sandboxing) - preferred\e[m"
+    echo -e "\e[0;35m3 - Install landrun (kernel-level sandboxing) - alternative\e[m"
+    echo -e "\e[0;35m4 - Install/Update scoder (opencode sandboxing env) - alternative\e[m"
+    echo -e "\e[0;35m5 - Install/Update configuration files (opencode & nono)\e[m"
 
     read -p 'Choice: ' ACTION
     
@@ -49,8 +52,8 @@ agentic_coding()
     if [[ "$ACTION" == 2 ]]
     then
         echo -e "\e[0;35m Installing nono (kernel-level sandboxing)...\e[m"
-        mkdir -p "${emacs_deps_dir}" && cd "${emacs_deps_dir}" && wget "${NONO_DOWNLOAD_URL}"
-        tar -xf "${NONO_BINARY}"
+        mkdir -p "${emacs_deps_dir}" && cd "${emacs_deps_dir}" && wget "${nono_download_url}"
+        tar -xf "${nono_binary}"
         mv nono "${HOME}/.local/bin/"
         echo -e "You may now invoke opencode with nono via: \n"
         echo -e "\e[0;35m nono run -vvv --profile opencode --read ~/.opencode --allow /dev -- opencode \e[m"
@@ -73,15 +76,19 @@ agentic_coding()
         sudo chmod +x /usr/local/bin/scoder
 
         # Fallback ABI check for Ubuntu 24.04.4 LTS
-        cp "${agentic_coding_path}/landlock_abi_check" "${LANDLOCK_ABI_CHECK_DEST}"
-        sudo chmod +x "${LANDLOCK_ABI_CHECK_DEST}/landlock_abi_check"
+        cp "${this_root_path}/landlock_abi_check" "${landlock_abi_check_dest}"
+        sudo chmod +x "${landlock_abi_check_dest}/landlock_abi_check"
     fi
 
     if [[ "$ACTION" == 5 ]]
     then
-        echo -e "\e[0;35m Installing/update opencode.json config...\e[m"
+        echo -e "\e[0;35m Installing/updating configuration files...\e[m"
         git pull
-        cp "${agentic_coding_path}/opencode.json" "${HOME}/.config/opencode/"
+        echo -e "\e[0;35m .config/opencode \e[m"
+        cp "${opencode_config}" "${HOME}/.config/opencode/"
+        echo -e "\e[0;35m .config/nono/profiles \e[m"
+        mkdir -p "${nono_profiles_dest}"
+        cp "${nono_opencode_config}" "${nono_profiles_dest}"
     fi    
     
 }
